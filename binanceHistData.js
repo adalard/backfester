81dde2831d0228f00917c393847bd97c3e4ff364
fester <syhester@gmail.com>
2018-05-07 20:38:03 +0700

    turned into a module

--- binanceHistData.js
+++ binanceHistData.js
@@ -1,30 +1,33 @@
-// Supported Intervals: 1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M
 const fs = require('fs');
-const now = new Date();
 var request = require('request-promise');
-const options = {
-    method: 'GET',
-    uri: 'https://api.binance.com/api/v1/klines?',
-    qs: {
-        symbol: 'BTCUSDT',
-        interval: '1h'
-    }
-};
 
-exports.getHistoricalData = function () {
+var getHistoricalData = (exchange, pair, interval, callback) => {
+    const options = {
+        method: 'GET',
+        uri: 'https://api.binance.com/api/v1/klines?',
+        qs: {
+            symbol: pair,
+            interval: interval
+        }
+    };
 request(options)
     .then(function (response) {
-        fs.writeFile(__dirname + '/data/' + "BIN_" + options.qs.symbol + "_" + options.qs.interval + ".json", (response), (err) => {
+        fs.writeFile(__dirname + '/data/' + exchange + "_" + pair + "_" + interval + ".json", (response), (err) => {
             if (err) {
-                console.error(err);
+                callback('File could not be created ...');
             };
-            console.log(now, "BIN_" + options.qs.symbol + "_" + options.qs.interval + ".json has been created :-)")
+            console.log("BIN_" + options.qs.symbol + "_" + options.qs.interval + ".json has been created :-)")
+            callback(undefined, {
+
+            })
         });
     })
     .catch(function (err) {
         console.log(err)
     })
 }
+
+module.exports.getHistoricalData = getHistoricalData;
 
 /* JSON Array formatting:
 
@@ -40,5 +43,9 @@ request(options)
  10:  "1756.87402397",    // Taker buy base asset volume
  11:  "28.46694368",      // Taker buy quote asset volume
  12:  "17928899.62484339" // Ignore
+
+
+
+ // Supported Intervals: 1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M
 
 */
