9cca5944a0c53adbe6186402dd0b3711b91e19d9
fester <syhester@gmail.com>
2018-05-09 17:00:49 +0700

    added buypercent

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const fs = require('fs'); // to access filesystem
-const data = require('./binanceHistData')
+const data = require('./binanceHistData') //module to get historical data
 const exchange = 'BIN';   // Supported exchange: BIN
 const pair = 'BTCUSDT';   // pairs to trade
 const interval = '1d';   // Supported Intervals: 1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M
+const sellpercent = 1.30; // how much to buy
+const buypercent =1.30;
 
 var coinBalance = 0;
 var historicalData = {}; // object to hold the json file data
 var openedPositions = 0; // holds number of trading positions open
 var openedPositionPrice = 0;
-var position = 0; // if position is open
-const sellpercent = 1.10;
+var position = 0; // if position is open, we start with no open positions
 
 function loadJson() {
     historicalData = JSON.parse(fs.readFileSync(__dirname + '/data/' + exchange + '_' + pair + '_' + interval + '.json', 'utf8'));
@@ -27,7 +28,7 @@ for (index = 1; index < historicalData.length; index++) {
         }
     }
     if (position == 0) { // if we dont have an open position look to buy
-        if (parseInt(historicalData[index][4]) < parseInt(historicalData[index-1][4])) { // if current price is less than previous
+        if (parseInt(historicalData[index][4]) < parseInt(historicalData[index-1][4] * buypercent)) { // if current price is less than previous
             openedPositionPrice = parseInt(historicalData[index][4]);
             console.log(historicalData[index][0], index, pair, 'bought at', openedPositionPrice);
             position = 1;
