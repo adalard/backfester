68c40f0eb92fb4ed09a58925268cc4ec5f442fb4
Simon Fester <syhester@gmail.com>
2018-09-19 13:06:15 +0000

    lots of changes really, mostly formatting

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const data = require('./binanceHistData') //module to get historical data
 const exchange = 'BIN';   // Supported exchange: BIN
 const pair = 'BTCUSDT';   // pairs to trade
 const interval = '1d';   // Supported Intervals: 1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M
-const sellpercent = 1.30; // how much to buy
-const buypercent =1.30;
+const sellpercent = 1.20; // how much to buy
+const buypercent = 1.20;
 
-var coinBalance = 1000;
+var funds = 1000;
 var historicalData = {}; // object to hold the json file data
 var openedPositions = 0; // holds number of trading positions open
-var openedPositionPrice = 0;
+var buyPrice = 0;
+var sellPrice = 0;
 var position = 0; // if position is open, we start with no open positions
 
 function loadJson() {
@@ -17,31 +18,33 @@ function loadJson() {
     console.log('JSON loaded')
     main();
 }
-
 function main() {
 for (index = 1; index < historicalData.length; index++) { 
-    if (position == 1) { // if we have an open position - look to sell
-        if (parseInt(historicalData[index][4]) >= (openedPositionPrice * sellpercent)) {
-            console.log(historicalData[index][0], index, pair, 'sold at', parseInt(historicalData[index][4]))
-            coinBalance = coinBalance + (historicalData[index][4] - openedPositionPrice)
+    if (position == 0) { // Sell
+        if (parseFloat(historicalData[index][4]) >= (buyPrice * sellpercent)) {
+            sellPrice = parseFloat(historicalData[index][4]);
+            var poo = new Date(historicalData[index][0]);
+            //funds = funds + sellPrice;
             position = 0;
+            console.log(poo + ', ' + pair + ', ' + 'SELL' + ', ' + sellPrice + ', ' + 'balance=', funds);
         }
     }
-    if (position == 0) { // if we dont have an open position look to buy
-        if (parseInt(historicalData[index][4]) < parseInt(historicalData[index-1][4] * buypercent)) { // if current price is less than previous
-            openedPositionPrice = parseInt(historicalData[index][4]);
-            console.log(historicalData[index][0], index, pair, 'bought at', openedPositionPrice);
+    if (position == 0) { // Buy
+        if (parseFloat(historicalData[index][4]) < parseFloat(historicalData[index-1][4] * buypercent)) { // if current price is less than previous
+            buyPrice = parseFloat(historicalData[index][4]);
+            var poo = new Date(historicalData[index][0]);
             position = 1;
             openedPositions ++ ;
+            console.log(poo + ', ' + pair + ', ' + 'BUY ' + ', ' + buyPrice + ', ' + 'balance= ' + funds);
         }
     }
 }
 console.log('Total positions opened', openedPositions);
-console.log('Total profit', coinBalance);
+console.log('End Balance', funds);
 console.log('Interval', interval);
 }
 
-data.getHistoricalData(exchange, pair, interval, (errorMessage, results) => { // need to improve error handling
+data.getHistoricalData(exchange, pair, interval, (errorMessage, results) => { // need to improve the error handling
     if (errorMessage) {
         console.log(errorMessage);
     } else {
